0b0035ed9aa907ab0b012220dd47f0dd90c466d0
kim1210 <69778811+kim1210@users.noreply.github.com>
2022-02-06 12:49:50 +0900

    add Login and Logout functionality

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Footer from "../footer/footer";
 import Header from "../header/header";
 import styles from "./login.module.css";
 
 const Login = ({ authService }) => {
+  const navigate = useNavigate();
+  const goToMaker = (userId) => {
+    navigate("/maker", { state: { id: userId } });
+  };
   const onLogin = (event) => {
     authService
       .login(event.currentTarget.textContent) //
-      .then(console.log);
+      .then((data) => goToMaker(data.user.uid));
   };
+  // 로그인 정보를 기억하고 그에 따라 goToMaker
+  useEffect(() => {
+    authService.onAuthChange((user) => {
+      user && goToMaker(user.uid);
+    });
+  });
   return (
     <section className={styles.login}>
       <Header />
@@ -16,10 +27,14 @@ const Login = ({ authService }) => {
         <h1>Login</h1>
         <ul className={styles.list}>
           <li className={styles.item}>
-            <button className={styles.button} onClick={onLogin}>Google</button>
+            <button className={styles.button} onClick={onLogin}>
+              Google
+            </button>
           </li>
           <li className={styles.item}>
-            <button className={styles.button} onClick={onLogin}>Github</button>
+            <button className={styles.button} onClick={onLogin}>
+              Github
+            </button>
           </li>
         </ul>
       </section>
---
+++ src/components/maker/maker.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import Footer from "../footer/footer";
+import Header from "../header/header";
+import styles from "./maker.module.css";
+
+const Maker = ({ authService }) => {
+  // 현재 페이지의 location object 확인
+  const location = useLocation();
+  console.log("state", location.state);
+
+  const navigate = useNavigate();
+  const onLogout = () => {
+    authService.logout();
+  };
+  useEffect(() => {
+    authService.onAuthChange((user) => {
+      if (!user) {
+        navigate("/");
+      }
+    });
+  });
+
+  return (
+    <section className={styles.maker}>
+      <Header onLogout={onLogout} />
+      <Footer />
+    </section>
+  );
+};
+
+export default Maker;
